174f0083ad6e0a287176247e6e2d84aca804d12a
Luis-Urso <36892674+Luis-Urso@users.noreply.github.com>
2023-01-10 19:26:01 -0300

    upsklilling scripts changes

--- Diversos/3D Array Load and Read.py
+++ Diversos/3D Array Load and Read.py
@@ -7,11 +7,16 @@ cx[0]=10
 cx[1]=20
 cx[2]=30
 
+print(buffer.shape)
+
 for i in range(0,10):
     for f in range(0,3):
         for g in range(0,3):
             buffer[i][0][0]=cx[0]
             buffer[i][1][1]=cx[1]
             buffer[i][2][2]=cx[2]
-            
-print(buffer)
+        
+        
+buffer = buffer[0:9,:,:]
+
+print(buffer.shape)
