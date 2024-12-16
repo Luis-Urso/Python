31e65e97ab5cfee74d2a5de87d9f07ccb621b3b6
Luis-Urso <36892674+Luis-Urso@users.noreply.github.com>
2024-12-16 10:50:41 -0300

    chess project

---
+++ Chess/chess_arena.py
@@ -0,0 +1,40 @@
+
+#%%
+import chess
+import chess.pgn
+
+board = chess.Board()
+board
+
+board.push_san("Nf3")
+
+board
+
+#%% Next Movement
+
+board.push_san("Nf6")
+
+board
+
+#%%
+
+board.push_san("g3")
+board
+
+# %%
+board.push_san("g6")
+board
+# %%
+board.push_san("Bh3")
+board
+
+# %%
+board.push_san("Ne4")
+board
+# %%
+board.push_san("Bd7")
+board
+# %%
+board.push_san("Qd7")
+board
+# %%
